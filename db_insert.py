3cbeeeff3cfe4293e107c8c2355ef5423b5175c7
masirof <41544743+masirof@users.noreply.github.com>
2025-01-12 10:40:07 +0000

    py devcontainer

--- db_insert.py
+++ db_insert.py
@@ -1,1 +1,11 @@
+import os
+import pymysql
+
+# MySQLDBとの接続
+# SQLAlchemyはORMで便利に操作できそうだが、SQLのクエリを学習したいため
+# PyMySQLを仕様
+
+# actionsのsecretsに登録した環境変数の呼び出し
+# API_KEY = os.environ.get("API_KEY ")
+
 print('hi')
